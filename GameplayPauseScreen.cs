d82577a4ac36bad17982afceec196b941fc9a366
nobbele <realnobbele@gmail.com>
2021-04-29 14:25:40 +0200

    Adds UI Panel

--- GameplayPauseScreen.cs
+++ GameplayPauseScreen.cs
@@ -10,10 +10,16 @@ namespace ShogiClient
         private GameplayScreenState gameplayState;
         private Texture2D background;
 
+        private UIPanel panel;
+
         public GameplayPauseScreen(Game1 game, GameplayScreenState gameplayState, Texture2D background) : base(game)
         {
             this.gameplayState = gameplayState;
             this.background = background;
+            panel = new UIPanel(game) {
+                Position = new Vector2(100, 100),
+                Size = new Vector2(700, 500),
+            };
         }
 
         public override void Update(GameTime gameTime, KeyboardState keyboardState, KeyboardState prevKeyboardState, MouseState mouseState, MouseState prevMouseState)
@@ -30,6 +36,7 @@ namespace ShogiClient
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(background, new Rectangle(Point.Zero, Game.WindowSize.ToPoint()), null, Color.White);
+            panel.Draw(spriteBatch);
             spriteBatch.DrawString(Resources.PieceFont, "Paused", Game.WindowSize / 2 - Resources.PieceFont.MeasureString("Paused") / 2, Color.White);
         }
     }
---
+++ UIPanel.cs
@@ -0,0 +1,25 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShogiClient
+{
+    public class UIPanel
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; set; }
+
+        private Texture2D tex;
+
+        public UIPanel(Game game) 
+        {
+            var color = Color.White;
+            tex = new Texture2D(game.GraphicsDevice, 1, 1);
+            tex.SetData(new Color[] { color });
+        }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tex, new Rectangle(Position.ToPoint(), Size.ToPoint()), null, new Color(Color.Black, 0.6f));
+        }
+    }
+}
