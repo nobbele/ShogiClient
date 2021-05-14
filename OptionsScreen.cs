87e2be47c5dcb7fae40e17b6450e2537a2bd3245
nobbele <realnobbele@gmail.com>
2021-05-14 13:51:57 +0200

    adds volume slider to options

--- OptionsScreen.cs
+++ OptionsScreen.cs
@@ -13,6 +13,9 @@ namespace ShogiClient
         private Texture2D background;
 
         private UIPanel panel;
+        private UIText titleLabel;
+        private UIText volumeLabel;
+        private UISlider volumeSlider;
 
         public OptionsScreen(Game1 game, GameResources resources, TState prevState, Texture2D background) : base(game)
         {
@@ -23,6 +26,36 @@ namespace ShogiClient
             {
                 Position = new Vector2(100, 100),
                 Size = Game.WindowSize - new Vector2(200, 200),
+            };
+
+            titleLabel = new UIText(resources)
+            {
+                Position = new Vector2(Game.WindowSize.X / 2, Game.WindowSize.Y / 5),
+                Text = "Options"
+            };
+
+            volumeLabel = new UIText(resources)
+            {
+                Position = new Vector2(Game.WindowSize.X / 2, Game.WindowSize.Y * 2 / 5 - 30),
+                Text = "Volume"
+            };
+            volumeSlider = new UISlider(resources)
+            {
+                Position = new Vector2(Game.WindowSize.X / 2, Game.WindowSize.Y * 2 / 5),
+                Size = new Vector2(100, 20),
+                Min = 0,
+                Max = 0.10f,
+            };
+            volumeSlider.SetCurrentValue(MediaPlayer.Volume);
+            volumeSlider.OnChange += p =>
+            {
+                System.Console.WriteLine($"Setting volume to {p}");
+                MediaPlayer.Volume = p;
+            };
+            volumeSlider.OnRelease += p =>
+            {
+                // TODO Save audio volume
+                System.Console.WriteLine($"Saving volume as {p}");
             };
         }
 
@@ -46,12 +79,17 @@ namespace ShogiClient
                 Game.SetCurrentScreen(screen, false);
                 return;
             }
+
+            volumeSlider.Update(gameTime, keyboardState, mouseState, prevMouseState);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(background, new Rectangle(Point.Zero, Game.WindowSize.ToPoint()), null, Color.White);
             panel.Draw(spriteBatch);
+            titleLabel.Draw(spriteBatch);
+            volumeLabel.Draw(spriteBatch);
+            volumeSlider.Draw(spriteBatch);
         }
     }
 }
